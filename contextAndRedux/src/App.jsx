641292a30de9cb0e796577a3b6289b16b70974aa
vrahul2108 <140827837+vrahul2108@users.noreply.github.com>
2025-08-23 03:38:38 +0530

    StateManagement Revision

---
+++ contextAndRedux/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <div className='bg-gray-600 p-4 text-white text-3xl text-center'>ContextAPI</div>
+      
+    </>
+  )
+}
+
+export default App
