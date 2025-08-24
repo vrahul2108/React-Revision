4c0beb9f821f7f198a5dda88a0a703456a4503ca
vrahul2108 <140827837+vrahul2108@users.noreply.github.com>
2025-08-25 03:40:38 +0530

    t Please enter the commit message for your changes. Lines starting

--- toDoContext/src/App.jsx
+++ toDoContext/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { TodoProvider } from './contexts'
+import { useEffect } from 'react'
+import TodoForm from './components/ToDoForm'
+import TodoItem from './components/ToDoItem'
 
 export default function App() {
 
@@ -21,8 +24,21 @@ export default function App() {
   }
 
   const toggleComplete =(id)=>{
-    setTodos((prev)=> prev.map((prevTodo)=> prevTodo===id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo) )
+    setTodos((prev)=> prev.map((prevTodo)=> prevTodo.id ===id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo) )
   }
+
+  useEffect( ()=>{
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.length >0){
+     setTodos(todos)
+    }
+  },[])
+
+  useEffect(()=>{
+   localStorage.setItem("todos", JSON.stringify(todos))
+  },[todos])
+
 
   return (
     <TodoProvider value={{todos, addTodo, updatedTodo, deleteTodo, toggleComplete}}>
@@ -30,10 +46,17 @@ export default function App() {
       <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
-              {/* Todo form goes here */} 
+              {/* Todo form goes here */}
+              <TodoForm/> 
           </div>
           <div className="flex flex-wrap gap-y-3">
               {/*Loop and Add TodoItem here */}
+              {todos.map((todo)=> (
+                <div key={todo.id} 
+                className='w-full '>
+                   <TodoItem todo = {todo}/>
+                </div>
+              ))}
           </div>
       </div>
   </div>
